b8c4e9c23c81d3cae5922e8d460c6e0896b409d6
zaa92reer@gmail.com <mohammad@Moh_Zar.localdomain>
2021-02-25 03:04:15 +0200

    finish js Part

--- js/cart.js
+++ js/cart.js
@@ -19,26 +19,50 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  let tbody = document.getElementsByTagName("tbody");
+  tbody[0].textContent = "";                               
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
-
+  let tbody = document.getElementsByTagName('tbody');
   // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  for(let i=0; i<cart.items.length;i++) {
+    let itemName = cart.items[i].product;
+    // TODO: Create a TR
+    let tr = document.createElement('tr');
+    // TODO: Create a TD for the delete link, quantity,  and the item
+    let td1 = document.createElement('td');
+    let deleteLink = document.createElement('button');
+    deleteLink.textContent = 'x';
+    deleteLink.setAttribute("id", itemName);
+    td1.appendChild(deleteLink);
+    let td2 = document.createElement('td');
+    td2.textContent = cart.items[i].quantity;
+    let td3 = document.createElement('td');
+    td3.textContent = cart.items[i].product;
+    
+    // TODO: Add the TR to the TBODY and each of the TD's to the TR
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
 
+    tbody[0].appendChild(tr);
+  }
 }
 
 function removeItemFromCart(event) {
-
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+  if(event.target.nodeName === 'BUTTON'){
+    cart.removeItem(event.target.id);
+  }
   // TODO: Save the cart back to local storage
+  cart.saveToLocalStorage();
   // TODO: Re-draw the cart table
-
+  renderCart();
 }
 
 // This will initialize the page and draw the cart on screen
